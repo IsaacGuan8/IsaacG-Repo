408e777854a5e09f0182d7131b76f77e5b044b5d
Isaac Guan <isaac@Isaacs-MacBook-Air.local>
2024-09-14 17:46:45 -0700

    Assignment3 commit

--- ClassLecture/ClassLecture/main.cpp
+++ ClassLecture/ClassLecture/main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 
-int main() {
-    std::cout<<"Enter three number";
-    int number1;
-    int number2;
-    int number3;
+int main(){
+    //int number = 255;
+    int number = 0b11111111;
     
-    std::cin >> number1 >> number2 >> number3;
-    std::cout<<"you entered "<<number1;
     return 0;
 }
 
