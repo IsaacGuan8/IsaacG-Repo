8f182a0fa5d8e9356cdd97d7f7029d12107a40d7
Isaac Guan <isaac@Isaacs-MacBook-Air.local>
2024-09-25 12:19:09 -0700

    initial commit

--- Assignment15/Assignment15/main.cpp
+++ Assignment15/Assignment15/main.cpp
@@ -1,14 +1,18 @@
-//
-//  main.cpp
-//  Assignment15
-//
-//  Created by Isaac Guan on 9/24/24.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int row;
+    
+    std::cout << "How many rows do you want?" << std::endl;
+    std::cin >> row;
+    std::cout << std::endl;
+    
+    for(int x = 1; x <= row ;x++)
+    {
+        for(int y = 1; y <= x; y++){
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
---
+++ Project 1 - Temperature Converter Plus/Project 1 - Temperature Converter Plus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project 1 - Temperature Converter Plus
+//
+//  Created by Isaac Guan on 9/24/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
