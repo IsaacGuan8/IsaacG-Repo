91d05468ccd8e2a75911cf05f005a02afc5015e2
Isaac Guan <isaac@Isaacs-MacBook-Air.local>
2024-09-27 17:06:59 -0700

    added global converter for celsius and Kelvin

--- Project 1 - Temperature Converter Plus/Project 1 - Temperature Converter Plus/main.cpp
+++ Project 1 - Temperature Converter Plus/Project 1 - Temperature Converter Plus/main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+
 double celsiusToFahrenheit(double celsius)
 {
     return (celsius * 9 / 5) + 32;
@@ -23,10 +24,72 @@ double kelvinToCelsius(double kelvin)
     return kelvin - 273.15;
 }
 
+void getUserInput(double& temp, char& unitIn, char& unitOut)
+{
+    cout << "what is the inital temperature: " << endl;
+    cin >> temp >> unitIn;
+    cout << endl;
+    cout << "select an output unit (Celsius: C, Fahrenheit: F, Kelvin: K)" << endl;
+    cin >> unitOut;
+}
+
+double kelvinGlobalConverter(double temperature, const char unit){
+
+    switch(unit)
+    {
+        case 'F': case 'f':
+            return celsiusToKelvin(kelvinToCelsius(temperature));
+            break;
+            
+        case 'C': case 'c':
+            return kelvinToCelsius(temperature);
+            break;
+            
+        default:
+            cout << "please, input a valid temperature (F or C)" << endl;
+            return 0;
+            break;
+    }
+}
+
+double celsiusGlobalConverter(double temperature, const char unit){
+    switch(unit)
+    {
+        case 'K': case 'k':
+            return celsiusToKelvin(temperature);
+            break;
+            
+        case 'F': case 'f':
+            return fahrenheitToCelsius(temperature);
+            break;
+            
+        default:
+            cout << "please, input a valid temperature (K or C)" << endl;
+            return 0;
+            break;
+    }
+}
+
+string getTemperatureUnit(char initial)
+{
+    if (initial == 'C'){
+        return " Celsius";
+    }
+    if (initial == 'F'){
+        return " Fahrenheit";
+    }
+    if (initial == 'K'){
+        return " Kelvin";
+    }
+    return "Unknown unit";
+}
+
 int main(){
     bool quit = false;
     int choice = 0;
     double temperature = 0;
+    char unitIn;
+    char unitOut;
     
     
     // brief introduction and intructions
@@ -37,44 +100,57 @@ int main(){
         // display menu
         cout << "(1) Celsius to Fahrenheit" << endl;
         cout << "(2) Fahrenheit to Celsius" << endl;
-        cout << "(3) Celsius to Kelvin " << endl;
+        cout << "(3) Celsius to Kelvin" << endl;
         cout << "(4) Kelvin to Fahrenheit" << endl;
-        cout << "(5) Quit\n" << endl;
-        cout << "Enter you selection:" << endl;
+        cout << "(5) Kelvin to any temperature" << endl;
+        cout << "(6) Celsius to any temperature" << endl;
+        cout << "(7) Quit\n" << endl;
+        cout << "Enter your selection:" << endl;
         cin >> choice;
         
         /*cout << << endl;*/
         
         switch(choice)
         {
-            case 1:
-                
+           case 1:
                 cout << "what is the temperature in Celsius?" << endl;
                 cin >> temperature;
                 cout << "\n" << temperature << " Celsius is " << celsiusToFahrenheit(temperature) << " in Fahrentheit\n" << endl;
                 break;
                 
             case 2:
-                
                 cout << "what is the temperature in Fahrenheit?" << endl;
                 cin >> temperature;
                 cout << temperature << " Fahrenheit is " << fahrenheitToCelsius(temperature) << " in Celsius\n" << endl;
                 break;
                 
             case 3:
-                
                 cout << "what is the temperature in Celsius?" << endl;
                 cin >> temperature;
                 cout << temperature << " celsius is " << celsiusToKelvin(temperature) << " in Kelvin\n" << endl;
                 break;
                 
             case 4:
-                
                 cout << "what is the temperature in Celsius?" << endl;
                 cin >> temperature;
                 cout << temperature << " Kelvin is " << kelvinToCelsius(temperature) << " in Celsius\n" << endl;
                 break;
+                
             case 5:
+                getUserInput(temperature, unitIn, unitOut);
+                cout << endl;
+                cout << temperature << getTemperatureUnit(unitIn) << " is " <<kelvinGlobalConverter(temperature, unitOut) << getTemperatureUnit(unitOut) << endl;
+                cout << endl;
+                break;
+                
+            case 6:
+                getUserInput(temperature, unitIn, unitOut);
+                cout << endl;
+                cout << temperature << getTemperatureUnit(unitIn) << " is " <<celsiusGlobalConverter(temperature, unitOut) << getTemperatureUnit(unitOut) << endl;
+                cout << endl;
+                break;
+                
+            case 7:
                 cout << "Thank you for using this calculator have a nice day!" << endl;
                 quit = true;
                 break;
