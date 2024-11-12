e82eb2a9967ff2e92bc5df506167c1ef3bcc6283
Isaac Guan <isaac@Isaacs-MacBook-Air.local>
2024-11-11 18:20:58 -0800

    fixing some minor issues

--- Assignment10/Assignment10/main.cpp
+++ Assignment10/Assignment10/main.cpp
@@ -10,9 +10,9 @@ int main() {
     cout << "Enter your sales amount: $";
     cin >> sales;
     
-    if(sales <= 150000) {
+    if(sales <= 100000) {
         commissionRate = 0.10;
-    } else if (sales <150000){
+    } else if (sales <= 150000){
         commissionRate = 0.15;
     } else {
         commissionRate = 0.20;
